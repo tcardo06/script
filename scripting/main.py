162283a77d554defe8e96638030a597f5f61cb97
Thomas_Cardoso <tcardo06@gmail.com>
2022-06-27 12:11:58 +0200

    add binary list

--- scripting/main.py
+++ scripting/main.py
@@ -5,13 +5,18 @@
 
 
 def print_value(name):
-    a_string = input("Entrer une valeur:")
+    base_string = input("Entrer une valeur:")
 
-    ASCII_values = []
-    for character in a_string:
-        ASCII_values.append(ord(character))
-    print(list(a_string))
-    print(ASCII_values)
+    list_ASCII = []
+    for character in base_string:
+        list_ASCII.append(ord(character))
+    print(list(base_string))
+    print(list_ASCII)
+
+    list_bin = []
+    for i in list_ASCII:
+        list_bin.append(bin(int(i))[2:])
+    print(list_bin)
 
 if __name__ == '__main__':
     print_value('PyCharm')
