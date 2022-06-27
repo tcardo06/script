16ad78b1245d20625f118f8c53124a7335e4527d
Thomas_Cardoso <tcardo06@gmail.com>
2022-06-27 15:03:31 +0200

    add list formatted with 6 digits each block

--- scripting/main.py
+++ scripting/main.py
@@ -27,5 +27,16 @@ def print_value(name):
     # Display list format Binary
     print(list_bin)
 
+    # String of raw binary characters
+    character_bin = ''.join(list_bin)
+    print(character_bin)
+
+    # n = length of one block
+    n = 6
+    # list formatted with 6 digits in each block
+    list_bin_formatted = [character_bin[i:i + n] for i in range(0, len(character_bin), n)]
+    print(list_bin_formatted)
+
+
 if __name__ == '__main__':
     print_value('PyCharm')
