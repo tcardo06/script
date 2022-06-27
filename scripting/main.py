4090c6cee173d98e2a380539a0b01d79d7e5eb39
Mel-LR <melyne.leray@edu.itescia.fr>
2022-06-27 12:34:37 +0200

    add comments

--- scripting/main.py
+++ scripting/main.py
@@ -5,17 +5,26 @@
 
 
 def print_value(name):
+    # The user must enter a value
     base_string = input("Entrer une valeur:")
 
+    # List of characters converted to Ascii
     list_ASCII = []
     for character in base_string:
         list_ASCII.append(ord(character))
+
+    # Display list
     print(list(base_string))
+
+    # Display list format Ascii
     print(list_ASCII)
 
+    # List of characters converted to Binary
     list_bin = []
     for i in list_ASCII:
         list_bin.append(bin(int(i))[2:].zfill(8))
+
+    # Display list format Binary
     print(list_bin)
 
 if __name__ == '__main__':
