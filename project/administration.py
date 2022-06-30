9722e2a35622ba5a79c7fd1053348d0fc280cc8b
Mel-LR <melyne.leray@edu.itescia.fr>
2022-06-30 15:06:15 +0200

    add argparse

--- project/administration.py
+++ project/administration.py
@@ -1,14 +1,20 @@
 from datetime import datetime, timedelta
 from sys import argv
-from time import time
+
+import time
 import argparse
 import logging
 
 import psutil
 
 
-logging.basicConfig(level=logging.INFO)
-logging.info('This will be logged')
+#logging.basicConfig(level=logging.INFO)
+#logging.info('This will be logged')
+
+
+
+NAME = argv[0]
+
 
 def system_info_all():
     info = {
@@ -37,12 +43,13 @@ def system_info_all():
         "Disk counters": f"{psutil.net_io_counters()}",
 
         # Others
-        "Uptime": timedelta(seconds=time() - psutil.boot_time()),
+        #"Uptime": timedelta(seconds=time() - psutil.boot_time()),
     }
-    #print("\n\n SYSTEM INFO ALL\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
+    print("\n\n SYSTEM INFO ALL\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
+
 
 def system_info_cpu():
-    info = {
+    info_cpu = {
         # CPU
         "Number of cores in system": f"{psutil.cpu_count(logical=False)}",
         "CPU Statistics": f"{psutil.cpu_stats()}",
@@ -52,17 +59,19 @@ def system_info_cpu():
         "Time on CPU": timedelta(seconds=psutil.cpu_times().system + psutil.cpu_times().user),
 
     }
-    print("\n\n SYSTEM INFO CPU\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
+    print("\n\n SYSTEM INFO CPU\n\n" + "\n".join([f"{key}: {value}" for key, value in info_cpu.items()]))
+
 
 def system_info_memory():
-    info = {
+    info_memory = {
         # Memory
         "Memory in use": f"{psutil.virtual_memory().percent}%",
         "Memory available": f"{psutil.virtual_memory().available / (1024 ** 3):,.3f} GiB",
         "Memory swap": f"{psutil.swap_memory()}",
 
     }
-    #print("\n\n SYSTEM INFO MEMORY\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
+    print("\n\n SYSTEM INFO MEMORY\n\n" + "\n".join([f"{key}: {value}" for key, value in info_memory.items()]))
+
 
 def system_info_networks():
     info = {
@@ -72,7 +81,8 @@ def system_info_networks():
         "Net stats": f"{psutil.net_if_stats()}",
 
     }
-    #print("\n\n SYSTEM INFO NETWORKS\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
+    print("\n\n SYSTEM INFO NETWORKS\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
+
 
 def system_info_disk():
     info = {
@@ -83,44 +93,7 @@ def system_info_disk():
         "Disk counters": f"{psutil.net_io_counters()}",
 
     }
-    #print("\n\n SYSTEM INFO DISK\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
-
-def system_info_others():
-    info = {
-        # Others
-        "Uptime": timedelta(seconds=time() - psutil.boot_time()),
-
-    }
-    #print("\n\n SYSTEM INFO OTHERS\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
-
-
-FUNCTION_MAP = {
-    'all': system_info_all(),
-    'cpu': system_info_cpu(),
-    'memory': system_info_memory(),
-    'networks': system_info_networks(),
-    'disk': system_info_disk(),
-    'others': system_info_others()
-    }
-
-system_info_all = argparse.ArgumentParser()
-system_info_all.add_argument("all", help="string to test", type=str, choices=FUNCTION_MAP.keys())
-system_info_all = system_info_all.parse_args()
-func = FUNCTION_MAP[system_info_all().all]
-func()
-
-print("The string is : " + system_info_all().all)
-
-system_info_cpu = argparse.ArgumentParser()
-system_info_cpu.add_argument("cpu", help="string to test", type=str, choices=FUNCTION_MAP.keys())
-system_info_cpu = system_info_cpu.parse_args()
-func = FUNCTION_MAP[system_info_cpu().cpu]
-func()
-
-print("The string is : " + system_info_cpu().cpu)
-
-
-NAME = argv[0]
+    print("\n\n SYSTEM INFO DISK\n\n" + "\n".join([f"{key}: {value}" for key, value in info.items()]))
 
 
 system_info_all()
@@ -128,4 +101,18 @@ system_info_cpu()
 system_info_memory()
 system_info_networks()
 system_info_disk()
-system_info_others()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--interval", help="echo the interval", type=int)
+args = parser.parse_args()
+
+while True:
+    time.sleep(args.interval)
+    system_info_all()
+    system_info_cpu()
+    system_info_memory()
+    system_info_networks()
+    system_info_disk()
+    print(args.interval)
+
